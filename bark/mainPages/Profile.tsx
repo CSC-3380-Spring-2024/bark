10c74a8be3034fc6475b6851e45a37392b7c1cfe
markstubbs426 <markjstubbs@icloud.com>
2024-03-17 14:48:58 -0500

    I have implemented a way to scroll through the profile and formatted it to look nice

--- bark/mainPages/Profile.tsx
+++ bark/mainPages/Profile.tsx
@@ -37,9 +37,11 @@ const styles = StyleSheet.create({
         fontSize: 35,
         color: 'purple'
     },
-    bioText: {
+    infoText: {
         fontSize: 20,
-        color: 'blue'
+        color: 'blue',
+        marginVertical:5,
+        alignSelf:'center',
     },
     dognouns:
     {
@@ -54,6 +56,11 @@ const styles = StyleSheet.create({
     image:{
         width:300,
         height:300,
+        borderBottomLeftRadius:10,
+        borderBottomRightRadius:10,
+        borderTopLeftRadius:10,
+        borderTopRightRadius:10,
+        marginVertical:5,
 
     },
     profilePic:{
@@ -65,6 +72,7 @@ const styles = StyleSheet.create({
         borderTopRightRadius:50,
         borderColor: 'black',
         borderWidth:3,
+        alignSelf: 'center',
     }
 });
 
@@ -77,7 +85,28 @@ const logo = {
   };
 const Profile = () => (
     <ScrollView>
-      <Image source = {require('./coolahhdog.jpg')} style = {styles.profilePic} ></Image>
+      {/* <View style={{flex: 7, flexDirection:'row'}}>
+        <Image source = {require('./coolahhdog.jpg')} style = {styles.profilePic} ></Image>
+      </View> */}
+        <View style={{flexDirection:'row'}}>
+                  <Image source = {require('./coolahhdog.jpg')} style = {styles.profilePic} ></Image>
+                  <View>
+                    <Text style ={styles.username}>CoolAhhDog21 </Text>
+                  <Text style ={styles.dognouns}> he/him and he/him </Text>
+                  </View>
+                  
+        </View>
+        <View style ={{alignSelf: 'center', marginVertical:5}}>
+            <Text style ={styles.infoText}>just some cool ahh dog fr </Text>
+            <Image source = {require("./coolahhdog.jpg")} style = {styles.image}></Image>
+            <Text style ={styles.infoText}>the realest and the baddest ;) </Text>
+            <Image source = {require("./corgi1.jpg")} style = {styles.image} ></Image>
+            <Text style ={styles.infoText}>no other dawgs be doing it like I do </Text>
+            <Image source = {require("./corgi2.jpg")} style = {styles.image} ></Image>
+            <Text style ={styles.infoText}>i bark and the bitches come running </Text>
+            <Image source = {require("./corgi3.jpg")} style = {styles.image} ></Image>
+        </View>
+      
     </ScrollView>
   );
   
