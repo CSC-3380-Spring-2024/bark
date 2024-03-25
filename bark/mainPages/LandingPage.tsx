7eb69fb3503040118c7b545d5a548fd515e0c8f9
rferrell03 <referrelliii@gmail.com>
2024-03-25 14:21:01 -0500

    Firebase realtimeDatabase setup

--- bark/mainPages/LandingPage.tsx
+++ bark/mainPages/LandingPage.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import {ActivityIndicator, Button, TextInput, View} from 'react-native'
-import { FIREBASE_AUTH } from '../FirebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
+import { FIREBASE_AUTH, FIREBASE_DATABASE } from '../FirebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, onAuthStateChanged } from '@firebase/auth';
+import {ref, set} from "@firebase/database";
 import { StyleSheet } from 'react-native';
 
 
@@ -10,8 +11,16 @@ export default function LandingPage(props: { setLoginStatus: (arg0: boolean) => 
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
     const auth = FIREBASE_AUTH;
+    const user = auth.currentUser;
 
+    onAuthStateChanged(auth, (user) => {
+        if(user){
+            const uid = user.uid;
+            console.log("User " + user.email + " with uid " + uid + " has logged in");
+        }
 
+    })
+    
     const signIn = async() =>{
         setLoading(true);
         try{
@@ -28,7 +37,13 @@ export default function LandingPage(props: { setLoginStatus: (arg0: boolean) => 
     const signUp = async() =>{
         setLoading(true);
         try{
-            const response = await createUserWithEmailAndPassword(auth, email, password);
+            const response = await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+                set(ref(FIREBASE_DATABASE, 'users/' + userCredential.user.uid), {
+                    username: email,
+                    email: email,
+                })
+            });
+            props.setLoginStatus(true);
             console.log(response)
         }catch(error){
             console.log(error)
---
+++ bark/mainPages/Onboarding.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+
+
+function Onboarding(){
+    return(
+        <View>
+            <Text>Onboarding Screen</Text>
+        </View>
+    );
+}
