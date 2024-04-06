f63dd922a8ea5143591f9541031e6e355f1431cc
Max Duet <mduet8@lsu.edu>
2024-04-06 15:35:40 -0500

    Made the structure for the general chat feature.

--- bark/mainPages/Chat.tsx
+++ bark/mainPages/Chat.tsx
@@ -1,9 +1,54 @@
-import {View, Text} from 'react-native'
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+const dogImg = require("../assets/silly dog.png");
 
-export default function Chat(){
-    return(
-        <View>
-            <Text>Chat screen</Text>
+export default function Chat() {
+  return (
+    <View>
+      {/* Pressable for the chat feature. */}
+      <Pressable style={styles.chatContainer}>
+        {/* Profile Picture */}
+        <View style={styles.pfpContainer}>
+          <Image style={styles.pfpStyle} source={dogImg}></Image>
         </View>
-    );
+        <View style={styles.textFlex}>
+          {/* Username */}
+          <View>
+            <Text style={styles.usernameText}>Username</Text>
+          </View>
+          {/* Last message sent */}
+          <View>
+            <Text style={styles.textFontStyle}>Recent message</Text>
+          </View>
+        </View>
+      </Pressable>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  chatContainer: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  pfpContainer: {
+    overflow: "hidden",
+    borderRadius: 100,
+    width: 75,
+    height: 75,
+  },
+  textFlex: {
+    flexDirection: "column",
+    paddingLeft: 10,
+    width: "100%",
+  },
+  pfpStyle: {
+    borderRadius: 100,
+    width: 75,
+    height: 75,
+  },
+  textFontStyle: {},
+  usernameText: {
+    fontWeight: "bold",
+    fontSize: 25,
+  },
+});
