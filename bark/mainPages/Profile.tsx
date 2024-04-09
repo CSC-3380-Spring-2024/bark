b5c5e8e828769da275cec202ba4b711f672c5cf5
markstubbs426 <markjstubbs@icloud.com>
2024-04-09 11:39:03 -0500

    made the profile look a bit more like how Julia had te other profiles set up with a horizontal scroll for the pictures

--- bark/mainPages/Profile.tsx
+++ bark/mainPages/Profile.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, ScrollView} from 'react-native'
+import {View, Text, StyleSheet, Image, ScrollView, Button, Alert, Pressable} from 'react-native'
 //import { ScrollView } from 'react-native-gesture-handler';
 import { blue } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+import Home from './Home';
 // import pic from '../../assets/adventuretime.png';
 
 // export default function Profile(){
@@ -31,22 +32,22 @@ import { blue } from 'react-native-reanimated/lib/typescript/reanimated2/Colors'
 // }
 
 
-
 const styles = StyleSheet.create({
     username: {
         fontSize: 35,
-        color: 'purple'
+        color: 'black',
+        flexWrap: 'wrap'
     },
     infoText: {
         fontSize: 20,
-        color: 'blue',
+        color: 'black',
         marginVertical:5,
         alignSelf:'center',
     },
     dognouns:
     {
         fontSize: 15,
-        color: 'purple'
+        color: 'black'
     },
     container:{
         flex:1,
@@ -54,13 +55,13 @@ const styles = StyleSheet.create({
         alignItems:'center'
     },
     image:{
-        width:300,
+        width:225,
         height:300,
         borderBottomLeftRadius:10,
         borderBottomRightRadius:10,
         borderTopLeftRadius:10,
         borderTopRightRadius:10,
-        marginVertical:5,
+        marginHorizontal: 5,
 
     },
     profilePic:{
@@ -73,41 +74,56 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth:3,
         alignSelf: 'center',
+        marginVertical:5
+    },
+    button:{
+        borderBlockColor: 'black',
+        borderColor: 'black',
+        backgroundColor: 'gainsboro',
+        borderBottomLeftRadius:10,
+        borderBottomRightRadius:10,
+        borderTopLeftRadius:10,
+        borderTopRightRadius:10,
+        marginRight: 5,
+        marginVertical:5
+    },
+    buttonText:{
+        fontSize: 20,
+        alignSelf: 'center',
+
     }
 });
 
-  
+type userProps = {
+    username: string,
+    dognouns: string,
+    loggedin: boolean,
+    bio: string,
+    dogNames: string,
+    humanName: string,
+    navigation: 
+}
 
-const logo = {
-    uri: 'https://reactnative.dev/img/tiny_logo.png',
-    width: 64,
-    height: 64,
-  };
-const Profile = () => (
-    <ScrollView>
-      {/* <View style={{flex: 7, flexDirection:'row'}}>
-        <Image source = {require('./coolahhdog.jpg')} style = {styles.profilePic} ></Image>
-      </View> */}
+const Profile = (props: userProps) => (
+    <View>
         <View style={{flexDirection:'row'}}>
                   <Image source = {require('./coolahhdog.jpg')} style = {styles.profilePic} ></Image>
-                  <View>
-                    <Text style ={styles.username}>CoolAhhDog21 </Text>
-                  <Text style ={styles.dognouns}> he/him and he/him </Text>
+                  <View style={{alignItems:'flex-start', marginHorizontal: 5}}>
+                    <Text style ={styles.username}>username </Text>
+                    <Text style ={styles.dognouns}>he/him and he/him </Text>
+                    <Pressable style = {styles.button}>
+                        <Text style={styles.buttonText}>Edit Profile</Text>
+                    </Pressable>
                   </View>
-                  
         </View>
-        <View style ={{alignSelf: 'center', marginVertical:5}}>
-            <Text style ={styles.infoText}>just some cool ahh dog fr </Text>
-            <Image source = {require("./coolahhdog.jpg")} style = {styles.image}></Image>
-            <Text style ={styles.infoText}>the realest and the baddest ;) </Text>
-            <Image source = {require("./corgi1.jpg")} style = {styles.image} ></Image>
-            <Text style ={styles.infoText}>no other dawgs be doing it like I do </Text>
-            <Image source = {require("./corgi2.jpg")} style = {styles.image} ></Image>
-            <Text style ={styles.infoText}>i bark and the bitches come running </Text>
-            <Image source = {require("./corgi3.jpg")} style = {styles.image} ></Image>
-        </View>
-      
-    </ScrollView>
+            <ScrollView  horizontal> 
+                <Image source = {require("./coolahhdog.jpg")} style = {styles.image}></Image>
+                <Image source = {require("./corgi1.jpg")} style = {styles.image} ></Image>
+                <Image source = {require("./corgi2.jpg")} style = {styles.image} ></Image>
+                <Image source = {require("./corgi3.jpg")} style = {styles.image} ></Image>
+            </ScrollView>            
+            <Text style ={styles.infoText}>bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio</Text>
+    </View>
   );
   
-  export default Profile;
+export default Profile;
