0ee15d50517fc1f6d2311a20df0d71b5d1016fc0
rferrell03 <referrelliii@gmail.com>
2024-04-16 11:06:58 -0500

    Created function generateNewProfiles to be ran upon app startup. This function returns an array of UID's which can be used to show new profiles

--- bark/mainPages/Other.tsx
+++ bark/mainPages/Other.tsx
@@ -1,9 +1,29 @@
-import {View, Text} from 'react-native'
-
+import {View, Text, Pressable} from 'react-native'
+import { ref, get } from '@firebase/database'
+import { FIREBASE_DATABASE } from '../FirebaseConfig';
 export default function Other(){
+    
+    const generateNewProfile = async() => {
+        const userRef = ref(FIREBASE_DATABASE, '/users')
+        const profiles : string[] = [];
+        await get(userRef).then((snapshot) =>{
+            if(snapshot.exists()){
+                console.log(snapshot.forEach((child) =>{
+                    profiles.push(child.key);
+                }));
+            }
+        }).finally(()=>{return profiles})
+
+    };
+
     return(
         <View>
             <Text>Other Screen</Text>
+            <Pressable onPress = {generateNewProfile}>
+                <View>
+                    <Text>Next Profile</Text>
+                </View>
+            </Pressable>
         </View>
     );
 }
