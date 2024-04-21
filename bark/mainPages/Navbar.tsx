7bd0917a42915c5fee241fa0448bc0ce516de8c7
Max Duet <mduet8@lsu.edu>
2024-04-21 11:21:07 -0500

    Added a skeleton for Firebase and started Navigation

--- bark/mainPages/Navbar.tsx
+++ bark/mainPages/Navbar.tsx
@@ -1,19 +1,20 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from './Home'
-import Profile from './Profile'
-import Chat from './Chat'
-import Other from './Other'
+import Home from "./Home";
+import Profile from "./Profile";
+import Chat from "./Chat";
+import Other from "./Other";
+import Texting from "./Texting";
 
 const Tab = createBottomTabNavigator();
 
-
-export default function Navbar(){
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name = "Home" component={Home} />
-            <Tab.Screen name = "Other" component = {Other} />
-            <Tab.Screen name = "Chat" component={Chat} />
-            <Tab.Screen name = "Profile" component={Profile} />
-        </Tab.Navigator>
-    )
+export default function Navbar() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Other" component={Other} />
+      <Tab.Screen name="Chat" component={Chat} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Texting" component={Texting} />
+    </Tab.Navigator>
+  );
 }
---
+++ bark/mainPages/Texting.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+
+export default function Texting(){
+    return(
+        <View>
+            <Text>Texting Screen</Text>
+        </View>
+    );
+}
