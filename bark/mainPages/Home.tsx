f9b45c382b93cc8762b68dbdd12cedacde96725a
rferrell03 <referrelliii@gmail.com>
2024-04-21 11:36:37 -0500

    Pulls profiles, and generates an order for them to display to the user

--- bark/mainPages/Home.tsx
+++ bark/mainPages/Home.tsx
@@ -1,26 +1,22 @@
-import {View, Text, Pressable} from 'react-native'
-import generateProfiles from '../components/generateProfiles';
+import { View, Text, Pressable } from "react-native";
+import generateProfiles from "../components/generateProfiles";
 
-
-export default function Home(){
-    const profiles: string[] = [];
-    async function assign(){
-        await generateProfiles().then((promise)=>{ 
-            promise.forEach((uid)=>{
-                profiles.push(uid)
-            })
-        })        
-    }
-
-    return(
-        <View>
-            <Text>Home Screen</Text>
-            <Pressable onPress={assign}>
-                <Text>Assign</Text>
-            </Pressable>
-            <Pressable onPress={print}>
-                <Text>Print that shit</Text>
-            </Pressable>
-        </View>
-    );
+export default function Home() {
+  const profiles: string[] = [];
+  async function generateProf() {
+    await generateProfiles()
+      .then((promise) => {
+        promise.forEach((uid) => {
+          profiles.push(uid);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
 }
