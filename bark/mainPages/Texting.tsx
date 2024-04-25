bab84e8cdda095957b164afb4498e5ba447196e0
Max Duet <mduet8@lsu.edu>
2024-04-25 06:23:11 -0500

    Added chat bubbles and fixed bugs

--- bark/mainPages/Texting.tsx
+++ bark/mainPages/Texting.tsx
@@ -1,19 +1,29 @@
 import {useState} from 'react';
-import {View, Text, StyleSheet, TextInput, Pressable, Keyboard, TouchableWithoutFeedback} from 'react-native'
+import {View, Text, StyleSheet, TextInput, Pressable, Keyboard, TouchableWithoutFeedback, KeyboardAvoidingView, ScrollView} from 'react-native'
 
 
 export default function Texting(){
-    const [text, onChangeText] = useState('Send message')
+    const [text, onChangeText] = useState('')
     const [height, setHeight] = useState(0)
     return(
+
+        <View>
+        {/* Makes the keyboard go above the text you are sending */}
+        <KeyboardAvoidingView behavior={'height'} keyboardVerticalOffset={90}>
+
+        {/* Makes the keyboard vanish when tapping somewhere else */}
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        
         <View style={styles.bigFlex}>
+
+            {/* Everything in the texting box */}
             <View style={styles.textingBox}>
-            <TextInput onChangeText={onChangeText} value={text} multiline={true}  
-            style={[styles.texting, {height: Math.max(35, height)}]} onContentSizeChange={(event) =>
-        setHeight(event.nativeEvent.contentSize.height)
+            <TextInput onChangeText={onChangeText}  placeholder = "Send your message here..." value={text} multiline={true}  
+            style={{height: Math.max(35, height),fontSize:20}} onContentSizeChange={(event) =>
+        setHeight(event.nativeEvent.contentSize.height) 
       } />
-              <View style={styles.sendContainer}>
+        {/* Send button */}
+        <View style={styles.sendContainer}>
             <Pressable>
                 <Text>
                     Send
@@ -21,48 +31,94 @@ export default function Texting(){
             </Pressable>
         </View>
         </View>
+
+
+
+        <View style={styles.chatFlex}>
+            <ScrollView>
+            {/* Chat Left Bubble */}
+            <View style={styles.chatLeft}>
+            <Text>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae totam culpa blanditiis atque itaque ullam quisquam, aliquam optio esse nulla.
+            </Text>
+            </View>
+            {/* Chat Right Bubble */}
+            <View style={styles.chatRight}>
+                <Text>
+                    Chat Right
+                </Text>
+            </View>
+            <View style={styles.chatLeft}>
+            <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, veniam accusantium. Aspernatur.
+            </Text>
+            </View>
+            <View style={styles.chatRight}>
+                <Text>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem beatae dignissimos laboriosam iusto, maxime nesciunt alias quaerat minus, assumenda, suscipit itaque natus unde?
+                </Text>
+            </View>
+            </ScrollView>
+        </View>
         </View>
         </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+        </View>
         
     );
 }
 
 const styles = StyleSheet.create({
-    texting:{
-
-    },
     sendContainer:{
         backgroundColor: "blue",
         borderRadius: 100,
         width: 75,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        height: 37,
+        position: "absolute", right:0
     },
     textingBox: {
         borderColor: "red",
         borderWidth: 1,
         marginTop: "auto",
         backgroundColor: "crimson",
-        borderRadius: 100,
+        borderRadius: 25,
         paddingLeft: 20,
         width: "90%",
         marginLeft:20,
         flexDirection: "row",
-        justifyContent: "space-between"
-
+        justifyContent: "space-between",
+        flexWrap: "nowrap"
     },
     bigFlex:{
         borderWidth: 1,
         width:"100%",
-        height: "100%"
+        height: "100%",
     },
-    textLeft:{ 
-
+    chatFlex:{
+        borderWidth: 1,
+        borderColor: "pink",
+        width: "100%",
+        height: "92.5%",
+        position: "absolute", top:0,
     },
-    textRight:{
-
+    chatLeft:{
+        borderRadius: 10,
+        backgroundColor: "darkkhaki",
+        maxWidth:"65%",
+        flexWrap: "nowrap",
+        margin: 10,
+        padding: 10
+    },
+    chatRight:{
+        borderRadius: 10,
+        backgroundColor: "darkseagreen",
+        maxWidth: "65%",
+        flexWrap: "nowrap",
+        margin: 10,
+        padding: 10,
+        alignSelf: "flex-end"
     }
-    //chat bubbles
-    //text box
-    //if text is sent, send right. if text is recieved, send left
+
 })
